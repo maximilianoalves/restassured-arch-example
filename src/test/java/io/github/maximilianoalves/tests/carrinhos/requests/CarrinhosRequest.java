080393f852756a6294c45f605d8bae9d4f6214b4
Maximiliano <maximilianodacruz@gmail.com>
2021-09-23 16:06:48 -0300

    chore: adicionando testes para o carrinho

--- src/test/java/io/github/maximilianoalves/tests/carrinhos/requests/CarrinhosRequest.java
+++ src/test/java/io/github/maximilianoalves/tests/carrinhos/requests/CarrinhosRequest.java
@@ -1,4 +1,22 @@
 package io.github.maximilianoalves.tests.carrinhos.requests;
 
+import io.github.maximilianoalves.payloads.LoginPayload;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
 public class CarrinhosRequest {
+
+    private final String PATH_CARRINHOS = "/carrinhos";
+
+    @Step("Listar todos os carrinhos cadastrados.")
+    public Response listar() {
+        return given()
+                .header("accept", "application/json")
+                .header("Content-Type", "application/json")
+                .when()
+                .get(PATH_CARRINHOS);
+    }
+
 }
--- src/test/java/io/github/maximilianoalves/tests/carrinhos/tests/CarrinhosTest.java
+++ src/test/java/io/github/maximilianoalves/tests/carrinhos/tests/CarrinhosTest.java
@@ -1,4 +1,27 @@
 package io.github.maximilianoalves.tests.carrinhos.tests;
 
-public class CarrinhosTest {
+import io.github.maximilianoalves.tests.base.tests.BaseTest;
+import io.github.maximilianoalves.tests.carrinhos.requests.CarrinhosRequest;
+import io.qameta.allure.Description;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.Matchers.lessThan;
+
+public class CarrinhosTest extends BaseTest {
+
+    CarrinhosRequest carrinhosRequest = new CarrinhosRequest();
+
+    @Test
+    @Tag("todos")
+    @Description("Deve listar todos os carrinhos com sucesso.")
+    public void deveListarTodosOsCarrinhosComSucesso() throws Exception {
+        carrinhosRequest.listar()
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .time(lessThan(2L), TimeUnit.SECONDS);
+    }
 }
