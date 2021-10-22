76ee477cc1b163c1af975c10a752bf876c2ac97c
Maximiliano <maximilianodacruz@gmail.com>
2021-10-22 15:50:56 -0300

    adicionando escolhar de ambiente

--- src/test/java/io/github/maximilianoalves/utils/Utils.java
+++ src/test/java/io/github/maximilianoalves/utils/Utils.java
@@ -1,6 +1,8 @@
 package io.github.maximilianoalves.utils;
 
 import com.github.javafaker.Faker;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 
 import java.util.Locale;
 
@@ -8,7 +10,42 @@ public class Utils {
 
     public static Faker faker = new Faker(new Locale("pt-BR"));
 
-    public static String getBaseUrl() {
-        return "https://serverest.dev";
+    public enum Env {
+        DEV, HML, PRD
     }
+
+    public static String getBaseUrl() {
+        String baseUrl = null;
+        Env env = envStringToEnum();
+
+        switch (env) {
+            case DEV:
+                baseUrl = "https://dev.serverest.dev";
+                break;
+            case HML:
+                baseUrl = "https://hml.serverest.dev";
+                break;
+            case PRD:
+                baseUrl = "https://serverest.dev";
+                break;
+            default:
+                Assert.fail("Ambiente não configurado");
+        }
+        return baseUrl;
+    }
+
+    public static Env envStringToEnum() {
+       try {
+           return Env.valueOf(getEnv());
+       } catch (IllegalArgumentException e) {
+           throw new IllegalArgumentException("Ambiente não existente: " + getEnv());
+       }
+    }
+
+
+    public static String getEnv() {
+        return System.getProperty("env").toUpperCase(Locale.ROOT);
+    }
+
+
 }
